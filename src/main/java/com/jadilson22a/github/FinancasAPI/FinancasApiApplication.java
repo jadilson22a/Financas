bd6b11d152da0f465d492802e0fd8af65d921532
jadilson22a <jadilsonalves777.ma@outlook.com>
2026-07-11 12:15:23 -0300

    iniciando projeto financasAPI

---
+++ src/main/java/com/jadilson22a/github/FinancasAPI/FinancasApiApplication.java
@@ -0,0 +1,13 @@
+package com.jadilson22a.github.FinancasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jadilson22a/github/FinancasAPI/FinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadilson22a.github.FinancasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
